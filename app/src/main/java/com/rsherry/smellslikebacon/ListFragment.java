edbee6d7ddcfa95f80585cfd6e7bfcb1c212a8f4
Ryan Sherry <rsherrycomputers@gmail.com>
2018-07-17 13:05:55 -0700

    added RecyclerView with click listener and toast

---
+++ app/src/main/java/com/rsherry/smellslikebacon/ListFragment.java
@@ -0,0 +1,34 @@
+package com.rsherry.smellslikebacon;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ListFragment extends Fragment {
+
+    public interface OnRecipeSelectedInterface {
+        void onListRecipeSelected(int index);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+
+        OnRecipeSelectedInterface listener = (OnRecipeSelectedInterface) getActivity();
+        View view = inflater.inflate(R.layout.fragment_list, container, false);
+
+        RecyclerView recyclerView = view.findViewById(R.id.listRecyclerView);
+        ListAdapter listAdapter = new ListAdapter(listener);
+        recyclerView.setAdapter(listAdapter);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+
+        return view;
+    }
+}
